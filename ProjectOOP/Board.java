6e48ad9817ae74e7662986023267278776c0b2a4
Nguyễn Phúc Khang <81841634+No1KhangNguyen@users.noreply.github.com>
2021-12-30 11:13:50 +0700

    Update Board.java

--- ProjectOOP/Board.java
+++ ProjectOOP/Board.java
@@ -74,7 +74,30 @@ public class Board extends JPanel implements ActionListener{
         }
         repaint();
     }
-
+    
+    public void paint(Graphics g){ 
+        super.paint(g);
+        Dimension size = getSize();
+        int boardTop = (int) size.getHeight() - BoardHeight * squareHeight();
+        for (int i = 0; i < BoardHeight; ++i) {
+            for (int j = 0; j < BoardWidth; ++j) {
+                Tetrominoes shape = shapeAt(j, BoardHeight - i - 1);
+                if (shape != Tetrominoes.NoShape)
+                    drawSquare(g, 0 + j * squareWidth(),
+                               boardTop + i * squareHeight(), shape);
+            }
+        }
+        if (curPiece.getShape() != Tetrominoes.NoShape) {
+            for (int i = 0; i < 4; ++i) {
+                int x = curX + curPiece.x(i);
+                int y = curY - curPiece.y(i);
+                drawSquare(g, 0 + x * squareWidth(),
+                           boardTop + (BoardHeight - y - 1) * squareHeight(),
+                           curPiece.getShape());
+            }
+        }
+    }
+    
     //Set Background to the game panel
     @Override
     protected void paintComponent(Graphics g){
@@ -141,3 +164,23 @@ public class Board extends JPanel implements ActionListener{
             repaint();
         }
      }
+    //Pieces'configuration
+    private void drawSquare(Graphics g, int x, int y, Tetrominoes shape){
+        Color colors[] = {
+        new Color(0,0,0), new Color(204, 102, 102), new Color(0,255,255),
+        new Color(102, 204, 102), new Color(102, 102, 204), new Color(0,255,128),
+        new Color(204, 204, 102), new Color(204, 102, 204), new Color(255,255,0),
+        new Color(102, 204, 204), new Color(218, 170, 0), new Color(255,51,153)
+    };
+        Color color = colors[shape.ordinal()];
+        g.setColor(color);
+        g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2);
+        g.setColor(color.brighter());
+        g.drawLine(x, y + squareHeight() - 1, x, y);
+        g.drawLine(x, y, x + squareWidth() - 1, y);
+        g.setColor(color.darker());
+        g.drawLine(x + 1, y + squareHeight() - 1,
+                         x + squareWidth() - 1, y + squareHeight() - 1);
+        g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1,
+                         x + squareWidth() - 1, y + 1);
+    }
